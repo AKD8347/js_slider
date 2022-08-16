86e273912e80d20a6d6e6c6155b733322a51b26a
Alexandr Dmitrenko <a.dmitrenko@trinitygroup.ru>
2022-08-16 14:28:31 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -12,40 +12,39 @@ function minusSlide() {
     showSlides(slideIndex -= 1);
 };
 
-//Определяем количество точек
+//Находим HTML-тег для отображения слайдов
 const slides = document.getElementsByClassName("slider__item");
+//Определяем HTML-тег для отображения точек, указывая что он первый в списке
 const dotsContainer = document.getElementsByClassName('slider__dots')[0];
 
-
+//Определяем количество точек
 function addDots() {
 
     for (let i = 0; i < slides.length; i++) {
+        //создаем тег li
         let dot = document.createElement('li');
+        //добавляем ему нужный класс
         dot.classList.add('slider__dots--item');
+        //добавляем тег в контейнер
         dotsContainer.appendChild(dot)
     }
-
 }
-
+//вызываем функцию
 addDots();
 
 // Основная функция слайдера
 function showSlides(n) {
-    let i;
     //определяем количество слайдов
     let slides = document.getElementsByClassName("slider__item");
-    let dots = document.getElementsByClassName('slider__dots--item');
+
     if (n > slides.length) {
         slideIndex = 1
     }
     if (n < 1) {
         slideIndex = slides.length
     }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('active');
     }
-    slides[slideIndex - 1].style.display = "block";
-    console.log(dots);
-    dots[slidesIndex - 1].classList.add('1');
-
+    slides[slideIndex - 1].classList.add('active');
 }

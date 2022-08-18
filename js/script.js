eff30ffc5fbc1c64743417bc7f9130a27d694f89
Alexandr Dmitrenko <a.dmitrenko@trinitygroup.ru>
2022-08-18 13:17:59 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -1,50 +1,68 @@
-// Индекс слайда по умолчанию
-let slideIndex = 1;
-showSlides(slideIndex);
+//Функция слайдера
+function showSlider () {
+    //Находим контейнер со слайдами
+    let sliderContainer = document.querySelector('.slider');
+    //Находим контейнер со стрелками
+    let sliderArrows = document.querySelector('.slider__arrows');
+    //Находим все слайды
+    let images = document.querySelectorAll('.slider__item');
 
-// Увеличиваем индекс на 1, показываем следующй слайд
-function plusSlide() {
-    showSlides(slideIndex += 1);
-}
+    /*Функция инициализвции картинок*/
+    initImages();
+    /*Функция инициализвции стрелок*/
+    initArrows();
 
-// Уменьшяет индекс на 1, показывает предыдущий слайд
-function minusSlide() {
-    showSlides(slideIndex -= 1);
-};
+    function initImages() {
+        //при отсутсвии картинок выходим из функции
+        if(!images) {
+            return;
+        }
+        //при наличии проходим по каждому элементу
+        images.forEach((img, index) => {
+            //добавляем элементу атрибут data-index с индексом элемента
+            img.setAttribute('data-index', index);
+            //добавляем элементу класс с с индексом элемента
+            img.classList.add('number-' + index);
+            //если элемент первый в списке
+            if (index === 0) {
+                //добавляем ему уктивный класс
+                img.classList.add('active');
+            } else {
+                //если нет - убираем уктивный класс
+                img.classList.remove('active');
+            }
+        })
+    }
 
-//Находим HTML-тег для отображения слайдов
-const slides = document.getElementsByClassName("slider__item");
-//Определяем HTML-тег для отображения точек, указывая что он первый в списке
-const dotsContainer = document.getElementsByClassName('slider__dots')[0];
+    function initArrows() {
+        //находим мтрелки и проходим по ним цыклом
+        sliderArrows.querySelectorAll('.slider__arrow').forEach(arrow => {
+            //каждому найденому элементу вешаем событие click
+            arrow.addEventListener('click', function () {
+              //определяем активный слайд и переводим значение в number
+              let activeSlide = +sliderContainer.querySelector('.active').dataset.index;
+              //определяем количество слайдов
+              let imgLength = images.length;
+              //в переменную nextSlide будем записывать номер следующего слайда
+              let nextSlide;
+              //при клике на стрелку влево
+              if(arrow.classList.contains('slider__arrow--prev')) {
+                  //если текуший слайд первый, мы идем к следую
+                  nextSlide = activeSlide === 0? imgLength - 1 : activeSlide - 1;
+              } else {
+                  nextSlide = activeSlide === imgLength - 1? 0 : activeSlide + 1;
+              }
+              runSlider(nextSlide);
+            });
+        })
+    }
 
-//Определяем количество точек
-function addDots() {
-
-    for (let i = 0; i < slides.length; i++) {
-        //создаем тег li
-        let dot = document.createElement('li');
-        //добавляем ему нужный класс
-        dot.classList.add('slider__dots--item');
-        //добавляем тег в контейнер
-        dotsContainer.appendChild(dot)
+    function runSlider(number) {
+        sliderContainer.querySelector('.active').classList.remove('active');
+        sliderContainer.querySelector('.number-' + number).classList.add('active');
     }
 }
-//вызываем функцию
-addDots();
 
-// Основная функция слайдера
-function showSlides(n) {
-    //определяем количество слайдов
-    let slides = document.getElementsByClassName("slider__item");
-
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].classList.remove('active');
-    }
-    slides[slideIndex - 1].classList.add('active');
-}
+document.addEventListener("DOMContentLoaded", function(){
+   showSlider();
+});

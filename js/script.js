388adc468abdb089dfbd7818c8277c3dad09b350
Alexandr Dmitrenko <a.dmitrenko@trinitygroup.ru>
2022-08-18 14:08:56 +0300

    add dots

--- js/script.js
+++ js/script.js
@@ -6,11 +6,15 @@ function showSlider () {
     let sliderArrows = document.querySelector('.slider__arrows');
     //Находим все слайды
     let images = document.querySelectorAll('.slider__item');
+    //Находим контейнер для точек
+    let dots = document.querySelector('.slider__dots');
 
-    /*Функция инициализвции картинок*/
+    //Функция инициализвции картинок
     initImages();
-    /*Функция инициализвции стрелок*/
+    //Функция инициализвции стрелок
     initArrows();
+    //Функция инициализвции тшчек
+    initDots()
 
     function initImages() {
         //при отсутсвии картинок выходим из функции
@@ -47,22 +51,61 @@ function showSlider () {
               let nextSlide;
               //при клике на стрелку влево
               if(arrow.classList.contains('slider__arrow--prev')) {
-                  //если текуший слайд первый, мы идем к следую
+                  //если текуший слайд первый, мы идем к следующему до последнего
                   nextSlide = activeSlide === 0? imgLength - 1 : activeSlide - 1;
               } else {
+                  //если текуший слайд последний, мы идем к первому
                   nextSlide = activeSlide === imgLength - 1? 0 : activeSlide + 1;
               }
+
+              if(activeSlide === 0 && activeSlide === activeSlide - 1) {
+                  nextSlide = activeSlide - 1;
+              } else if (activeSlide === activeSlide - 1) {
+                  nextSlide = activeSlide + 1;
+              }
+
+              //Функция переключения слайдера, в которую передаем номер акирвного слайда
               runSlider(nextSlide);
             });
         })
     }
 
+    function initDots() {
+        //при отсутсвии картинок выходим из функции
+        if(!images) {
+            return;
+        }
+        //при наличии проходим по каждому элементу
+        images.forEach((img, index) => {
+            //создаем верстку точки
+            let dot = `<li class="slider__dots--item number-${index} ${index === 0? 'active': ''}" data-index="${index}"></li>`;
+            //добавляем верстку в контейнер
+            dots.innerHTML += dot;
+
+        })
+
+        dots.querySelectorAll('.slider__dots--item').forEach(dot =>  {
+            dot.addEventListener('click', function (){
+                runSlider(this.dataset.index)
+                dots.querySelector('.active').classList.remove('active');
+                this.classList.add('active');
+            });
+        })
+    }
+
     function runSlider(number) {
+        //убираем у всех изображений активный класс
         sliderContainer.querySelector('.active').classList.remove('active');
+        //добавляем активный класс нужному изображению
         sliderContainer.querySelector('.number-' + number).classList.add('active');
+        //убираем у всех точек активный класс
+        dots.querySelector('.active').classList.remove('active');
+        //добавляем активный класс нужной точке
+        dots.querySelector('.number-' + number).classList.add('active');
     }
 }
 
+//при загрузке всех элементов страницы инициализируем слайдер
 document.addEventListener("DOMContentLoaded", function(){
    showSlider();
 });
